53301ff13f495163c5221278f01f65df6cb33bab
Nick Barrett <nick@oxygem.com>
2013-08-03 01:35:26 +0100

    Ignore files

--- index.php
+++ index.php
@@ -28,10 +28,18 @@
 		return $tmpl->showError( 'Request not set' );
 	//make request only certain chars
 	$_GET['request'] = preg_replace( '/[^aA-zZ0-9\/-]/i', '', $_GET['request'] );
-	
+
+	//request to be ignored?
+	if( in_array( $_GET['request'], $config['ignore'] ) )
+		return $tmpl->showError( 'File not found' );
+
 	//empty request = config/home
 	if( empty( $_GET['request'] ) )
 		$_GET['request'] = $config['home'];
+
+	//request a folder? get index
+	if( is_dir( './src/doc/' . $_GET['request'] ) )
+		$_GET['request'] .= substr( $_GET['request'], -1 ) == '/' ? 'index' : '/index';
 
 	//does this file exist?
 	$file = './src/doc/' . $_GET['request'] . '.md';
@@ -51,6 +59,7 @@
 	$files = glob( $folder . '/*.md' );
 	foreach( $files as $key => $f ):
 		$files[$key] = str_replace( array( $folder . '/', '.md' ), '', $f );
+		if( in_array( $files[$key], $config['ignore'] ) ) unset( $files[$key] );
 	endforeach;
 	$tmpl->setData( 'filenav', $files );
 	//get folders
@@ -62,7 +71,7 @@
 	//if request has a slash, up folder link
 	if( preg_match( '/\//', $_GET['request'] ) )
 		$tmpl->setData( 'subfolder', true );
-	
+
 	//get file
 	$data = file_get_contents( $file );
 
@@ -93,6 +102,9 @@
 		endforeach;
 		$data = str_replace( '#=index', $string, $data );
 	endif;
+
+	//another hack! (#=home become $index)
+	$data = str_replace( '#=home', $index, $data );
 
 	//make markdown
 	$data = $md->defaultTransform( $data );
